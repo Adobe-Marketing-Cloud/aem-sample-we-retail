4f0fceeec1d930e5a8caf975a7b3fce3e8412ad8
lrejmer <luke.rejmer@gmail.com>
2015-11-24 17:41:22 +0100

    added options in hero-image dialog to set full width layout and 16:9 aspect ratio

--- ui.apps/src/main/content/jcr_root/apps/we-retail/components/content/hero-image/hero-image.js
+++ ui.apps/src/main/content/jcr_root/apps/we-retail/components/content/hero-image/hero-image.js
@@ -1,5 +1,22 @@
 'use strict';
 
 use(['/libs/wcm/foundation/components/image/v2/image/image.js'], function (image) {
-    return image;
+    var CONST = {
+        FULL_WIDTH: 'useFullWidth',
+        KEEP_RATIO: 'keepRatio'
+    };
+
+    var classList = 'we-HeroImage';
+
+    if (granite.resource.properties[CONST.FULL_WIDTH] == 'true') {
+        classList += ' width-full';
+    }
+    if (granite.resource.properties[CONST.KEEP_RATIO] == 'true') {
+        classList += ' ratio-16by9';
+    }
+
+    return {
+        classList: classList,
+        image: image
+    };
 });
