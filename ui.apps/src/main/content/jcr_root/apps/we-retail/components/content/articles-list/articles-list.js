4538904892550e90eacb2ea5c620208b06aaf20b
Daniel Wagner <dawagner@adobe.com>
2015-12-02 08:56:58 +0100

    Added articles list script
    Former-commit-id: 406af3adcf57d3e5d1c3b5ef98306f7c3d811c9a

---
+++ ui.apps/src/main/content/jcr_root/apps/we-retail/components/content/articles-list/articles-list.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var global = this;
+
+/**
+ *
+ */
+use(["/libs/wcm/foundation/components/utils/AuthoringUtils.js",
+     "/libs/wcm/foundation/components/utils/ResourceUtils.js",
+     "/libs/sightly/js/3rd-party/q.js"], function (AuthoringUtils, ResourceUtils, Q) {
+
+    var ret = {
+        },
+        articles = this.articles;
+
+
+    return ret;
+});
