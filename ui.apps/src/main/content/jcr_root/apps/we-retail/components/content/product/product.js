0b4015fef8f849b4f01462c9399f7d62e02977d1
lrejmer <luke.rejmer@gmail.com>
2015-12-03 19:31:11 +0100

    updated product component; added Vue.js to clientlibs
    Former-commit-id: 9e642e570f416d36ada38368a7311014e303bfa6

--- ui.apps/src/main/content/jcr_root/apps/we-retail/components/content/product/product.js
+++ ui.apps/src/main/content/jcr_root/apps/we-retail/components/content/product/product.js
@@ -1,20 +1,87 @@
 'use strict';
-use(function () {
+
+use(['/etc/clientlibs/granite/lodash/modern/source/lodash-2.4.1.js'], function (_) {
     var product = {};
     var resolver = resource.getResourceResolver();
     var commerceService = resource.adaptTo(com.adobe.cq.commerce.api.CommerceService);
     var commerceSession = commerceService.login(request, response);
     var productPath = currentPage.getProperties().get("cq:productMaster", java.lang.String);
-    var productResource = resolver.getResource(productPath);
-    var prod = commerceService.getProduct(productPath);
-    var productData = productResource.adaptTo(org.apache.sling.api.resource.ValueMap);
-    var imageResource = resolver.getResource(currentPage.getProperties().get("cq:productMaster", java.lang.String) + "/image");
+    var baseProduct = commerceService.getProduct(productPath);
 
-    product.image = imageResource.adaptTo(org.apache.sling.api.resource.ValueMap).get("fileReference", java.lang.String);
-    product.name = productData.get("jcr:title", java.lang.String);
-    product.description = productData.get("jcr:description", java.lang.String);
-    product.summary = productData.get("summary", java.lang.String);
-    product.features = productData.get("features", java.lang.String);
-    product.price = commerceSession.getProductPrice(prod);
+    var baseProductProperties = getProductProperties(baseProduct);
+
+    var variants = [];
+
+    var variationAxis = baseProduct.getProperty("cq:productVariantAxes", java.lang.String);
+    var variationTitle = baseProduct.getProperty("variationTitle", java.lang.String);
+    var variationLead = baseProduct.getProperty("variationLead", java.lang.String);
+
+    var variations = {
+        type: variationAxis,
+        colors: {},
+        sizes: []
+    };
+
+    if (variationAxis) {
+        var unorderedVariations = baseProduct.getVariants();
+
+        while (unorderedVariations.hasNext()) {
+            var productVariation = unorderedVariations.next();
+
+            var variation = getProductProperties(productVariation);
+
+            if (variationLead !== '' && productVariation.getProperty(variationAxis, java.lang.String) === variationLead) {
+                variants.unshift(variation);
+            }
+            else {
+                variants.push(variation);
+            }
+
+            if ('' + variations.type === 'color') {
+                if (!variations.colors[variation.color.toLowerCase()]) {
+                    variations.colors[variation.color.toLowerCase()] = [];
+                }
+                variations.colors[variation.color.toLowerCase()].push(variation);
+            }
+            else if ('' + variations.type === 'size') {
+                variations.sizes.push(variation);
+            }
+        }
+    }
+
+    if (!variants.length) {
+        variants.push(baseProductProperties);
+    }
+    else {
+        baseProductProperties = variants[0];
+    }
+
+    product.base = baseProductProperties;
+    product.variants = variants;
+    product.variations = variations;
+
+    product.variationTitle = variationTitle;
+    product.variationLead = variationLead;
+
     return product;
+
+    function getProductProperties(product) {
+        var productImage = resolver.getResource(product.getImage().getPath());
+
+        return {
+            path: product.getPath(),
+            variants: product.getVariantAxes(),
+            sku: product.getSKU(),
+            title: product.getTitle(),
+            description: product.getDescription(),
+            color: product.getProperty('color', java.lang.String),
+            colorClass: ('' + product.getProperty('color', java.lang.String)).toLowerCase(),
+            size: product.getProperty('size', java.lang.String),
+            price: commerceSession.getProductPrice(product),
+            summary: product.getProperty('summary', java.lang.String),
+            features: product.getProperty("features", java.lang.String),
+            image: productImage != null ?
+                productImage.adaptTo(org.apache.sling.api.resource.ValueMap).get("fileReference", java.lang.String) : ""
+        };
+    }
 });
