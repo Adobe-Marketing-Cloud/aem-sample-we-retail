ca737b0e526b174691a7506a7820c9f6ad6b36ac
Daniel Wagner <dawagner@adobe.com>
2016-02-15 13:41:18 +0100

    Added comments component based on cq/social

---
+++ ui.apps/src/main/content/jcr_root/apps/we-retail/components/hbs/comments/clientlibs/js/comments.js
@@ -0,0 +1,24 @@
+/*
+ *
+ * ADOBE CONFIDENTIAL
+ * __________________
+ *
+ *  Copyright 2013 Adobe Systems Incorporated
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Adobe Systems Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Adobe Systems Incorporated and its
+ * suppliers and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Adobe Systems Incorporated.
+ */
+(function(SCF) {
+    "use strict";
+
+    SCF.registerComponent('we-retail/components/hbs/comments/comment', SCF.Comment, SCF.CommentView);
+    SCF.registerComponent('we-retail/components/hbs/comments', SCF.CommentSystem, SCF.CommentSystemView);
+
+})(SCF);
