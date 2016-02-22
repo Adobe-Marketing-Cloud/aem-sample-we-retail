0eb65dde03fe4e7cf2dd3c15f6196e6c849d516a
lrejmer <luke.rejmer@gmail.com>
2016-02-22 16:34:45 +0100

    added animation to submenu
    Former-commit-id: e20b075fdd2e7600f9e256a0f4cda758a5a9d987

--- ui.apps/src/main/content/jcr_root/apps/we-retail/components/structure/header/clientlib/js/subitems.js
+++ ui.apps/src/main/content/jcr_root/apps/we-retail/components/structure/header/clientlib/js/subitems.js
@@ -1,7 +1,13 @@
 (function ($) {
     $(function () {
+        var $navbarShutter = $('.navbar-absolute-top .navbar-shutter');
+
         $('.navbar-absolute-top').on('mouseenter mouseleave', '.navbar-center li:has(.navbar-nav-subitems)', function (e) {
             e.delegateTarget.classList.toggle('submenu-opened');
+            $navbarShutter.height(
+                e.delegateTarget.classList.contains('submenu-opened') ?
+                    $(e.target).next('.navbar-nav-subitems').innerHeight(true) : 0
+            );
         });
     });
 })(jQuery);
