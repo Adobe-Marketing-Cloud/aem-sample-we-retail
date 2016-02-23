6bc7866cc44fb6d4ea66867c267d9a19e869f29b
Daniel Wagner <dawagner@adobe.com>
2016-02-23 13:10:52 +0100

    Reduced viewport viewport check to left/right in products grid

--- ui.apps/src/main/content/jcr_root/apps/we-retail/components/content/products-grid/clientlib/js/products-grid.js
+++ ui.apps/src/main/content/jcr_root/apps/we-retail/components/content/products-grid/clientlib/js/products-grid.js
@@ -5,9 +5,7 @@
             rect = el.getBoundingClientRect();
 
         return (
-            rect.top >= 0 &&
             rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
             rect.right <= (window.innerWidth || document.documentElement.clientWidth)
         );
     }
