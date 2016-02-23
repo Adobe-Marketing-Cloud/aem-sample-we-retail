421a7e7424776250e42ca72e71f25684acc9bfdd
lrejmer <luke.rejmer@gmail.com>
2016-02-23 13:19:22 +0100

    fixed navbar-default styles; fixed toggling classes
    Former-commit-id: cf6325a6d6ea1eb927cb8908544ea23b88f10cb8

--- ui.apps/src/main/content/jcr_root/apps/we-retail/components/structure/header/clientlib/js/subitems.js
+++ ui.apps/src/main/content/jcr_root/apps/we-retail/components/structure/header/clientlib/js/subitems.js
@@ -2,12 +2,14 @@
     $(function () {
         var $navbarShutter = $('.navbar-absolute-top .navbar-shutter');
 
-        $('.navbar-absolute-top').on('mouseenter mouseleave', '.navbar-center li:has(.navbar-nav-subitems)', function (e) {
-            e.delegateTarget.classList.toggle('submenu-opened');
-            $navbarShutter.height(
-                e.delegateTarget.classList.contains('submenu-opened') ?
-                    $(e.target).next('.navbar-nav-subitems').innerHeight(true) : 0
-            );
-        });
+        $('.navbar-absolute-top')
+            .on('mouseenter', '.navbar-center li:has(.navbar-nav-subitems)', function (e) {
+                e.delegateTarget.classList.add('submenu-opened');
+                $navbarShutter.height($(e.target).next('.navbar-nav-subitems').innerHeight(true));
+            })
+            .on('mouseleave', '.navbar-center li:has(.navbar-nav-subitems)', function (e) {
+                e.delegateTarget.classList.remove('submenu-opened');
+                $navbarShutter.height(0);
+            });
     });
 })(jQuery);
