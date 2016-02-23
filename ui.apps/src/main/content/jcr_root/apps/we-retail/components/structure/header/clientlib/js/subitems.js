492257fc7e2f128743cdc5db88c29a4206eba177
lrejmer <luke.rejmer@gmail.com>
2016-02-23 13:32:31 +0100

    fixed keeping underline for selected section

--- ui.apps/src/main/content/jcr_root/apps/we-retail/components/structure/header/clientlib/js/subitems.js
+++ ui.apps/src/main/content/jcr_root/apps/we-retail/components/structure/header/clientlib/js/subitems.js
@@ -4,10 +4,12 @@
 
         $('.navbar-absolute-top')
             .on('mouseenter', '.navbar-center li:has(.navbar-nav-subitems)', function (e) {
+                e.target.classList.add('hover');
                 e.delegateTarget.classList.add('submenu-opened');
                 $navbarShutter.height($(e.target).next('.navbar-nav-subitems').innerHeight(true));
             })
             .on('mouseleave', '.navbar-center li:has(.navbar-nav-subitems)', function (e) {
+                (e.target.tagName === 'UL' ? e.target.previousElementSibling : e.target).classList.remove('hover');
                 e.delegateTarget.classList.remove('submenu-opened');
                 $navbarShutter.height(0);
             });
