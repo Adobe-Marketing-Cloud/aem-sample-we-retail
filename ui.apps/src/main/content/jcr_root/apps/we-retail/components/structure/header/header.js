fd5770c0280b1e07ffc02d478369a8b389d9eaa8
Daniel Wagner <dawagner@adobe.com>
2016-02-29 13:45:07 +0100

    #27 Added language switcher logic + styles
    Former-commit-id: 1f2798e2f75f835485240581b2dc0447cfd96537

--- ui.apps/src/main/content/jcr_root/apps/we-retail/components/structure/header/header.js
+++ ui.apps/src/main/content/jcr_root/apps/we-retail/components/structure/header/header.js
@@ -21,6 +21,7 @@ use(function () {
     var currentNav = currentPage.getAbsoluteParent(4);
     var currentNavPath = currentNav && currentNav.getPath();
     var languageRoot = "#";
+    var languages = [];
 
     var getPages = function(_root, level) {
         if (level === 0) {
@@ -41,7 +42,31 @@ use(function () {
         }
 
         return _items;
-    }
+    };
+
+    var getLanguages = function(root) {
+        var items = [], page, selected,
+            rootPath = root.getPath();
+
+        var parent = root.getParent();
+        if (parent == null) {
+            return [];
+        }
+        var it = parent.listChildren(new Packages.com.day.cq.wcm.api.PageFilter());
+
+        while (it.hasNext()) {
+            page = it.next();
+
+            items.push({
+                path: page.getPath(),
+                code: page.getName(),
+                name: page.getTitle(),
+                selected: rootPath.equals(page.getPath())
+            });
+        }
+
+        return items;
+    };
 
     if (root) {
         items = getPages(root, 2);
@@ -49,6 +74,8 @@ use(function () {
         if (root.path.substring(0, 6) != "/conf/") {
             languageRoot = root.path + ".html";
         }
+
+        languages = getLanguages(root);
     }
 
 
@@ -57,6 +84,7 @@ use(function () {
     return {
         items: items,
         theme: theme,
-        languageRoot: languageRoot
+        languageRoot: languageRoot,
+        languages: languages
     };
 });
