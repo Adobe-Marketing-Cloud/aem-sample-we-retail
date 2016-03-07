381c94e5ffd75e6bf2e92be713c81bca60b2d1e2
Daniel Wagner <dawagner@adobe.com>
2016-03-07 18:04:05 +0100

    Prevent npe when product is not set

--- ui.apps/src/main/content/jcr_root/apps/we-retail/components/content/product/product.js
+++ ui.apps/src/main/content/jcr_root/apps/we-retail/components/content/product/product.js
@@ -9,86 +9,87 @@ use(["commerce_init.js"], function (commerceInit) {
     var productPath = currentPage.getProperties().get("cq:productMaster", java.lang.String);
     var baseProduct = commerceService.getProduct(productPath);
     var redirect, errorRedirect, addToCartUrl;
-
-    var baseProductProperties = getProductProperties(baseProduct);
-
     var variants = [];
 
-    var variationAxis = baseProduct.getProperty("cq:productVariantAxes", java.lang.String);
-    var variationTitle = baseProduct.getProperty("variationTitle", java.lang.String);
-    var variationLead = baseProduct.getProperty("variationLead", java.lang.String);
+    if (baseProduct) {
+        var baseProductProperties = getProductProperties(baseProduct);
+        var variationAxis = baseProduct.getProperty("cq:productVariantAxes", java.lang.String);
+        var variationTitle = baseProduct.getProperty("variationTitle", java.lang.String);
+        var variationLead = baseProduct.getProperty("variationLead", java.lang.String);
 
-    var variations = {
-        type: variationAxis,
-        colors: {},
-        sizes: []
-    };
+        var variations = {
+            type: variationAxis,
+            colors: {},
+            sizes: []
+        };
 
-    if (request && request.getAttribute) {
-        addToCartUrl = request.getAttribute("cq.commerce.addToCartUrl");
-        redirect = request.getAttribute("cq.commerce.redirect");
-        errorRedirect = request.getAttribute("cq.commerce.errorRedirect");
-        baseProduct = request.getAttribute("cq.commerce.product");
+        if (request && request.getAttribute) {
+            addToCartUrl = request.getAttribute("cq.commerce.addToCartUrl");
+            redirect = request.getAttribute("cq.commerce.redirect");
+            errorRedirect = request.getAttribute("cq.commerce.errorRedirect");
+            baseProduct = request.getAttribute("cq.commerce.product");
 
-        if (baseProduct
-                && baseProduct.getImage()) {
-            baseProductImagePath = baseProduct.getImage().getPath();
-        }
-    }
-
-    if (resource && resource.getResourceResolver) {
-        var resolver = resource.getResourceResolver();
-        redirect = resolver.map(request, redirect);
-        errorRedirect = resolver.map(request, redirect);
-
-        if (baseProductImagePath) {
-            baseProductImagePath = resolver.map(baseProductImagePath);
-        }
-    }
-
-    if (variationAxis) {
-        var unorderedVariations = baseProduct.getVariants();
-
-        while (unorderedVariations.hasNext()) {
-            var productVariation = unorderedVariations.next();
-
-            var variation = getProductProperties(productVariation);
-
-            if (variationLead !== '' && productVariation.getProperty(variationAxis, java.lang.String) === variationLead) {
-                variants.unshift(variation);
+            if (baseProduct
+                    && baseProduct.getImage()) {
+                baseProductImagePath = baseProduct.getImage().getPath();
             }
-            else {
-                variants.push(variation);
-            }
+        }
 
-            if ('' + variations.type === 'color') {
-                if (!variations.colors[variation.color.toLowerCase()]) {
-                    variations.colors[variation.color.toLowerCase()] = [];
+        if (resource && resource.getResourceResolver) {
+            var resolver = resource.getResourceResolver();
+            redirect = resolver.map(request, redirect);
+            errorRedirect = resolver.map(request, redirect);
+
+            if (baseProductImagePath) {
+                baseProductImagePath = resolver.map(baseProductImagePath);
+            }
+        }
+
+        if (variationAxis) {
+            var unorderedVariations = baseProduct.getVariants();
+
+            while (unorderedVariations.hasNext()) {
+                var productVariation = unorderedVariations.next();
+
+                var variation = getProductProperties(productVariation);
+
+                if (variationLead !== '' && productVariation.getProperty(variationAxis, java.lang.String) === variationLead) {
+                    variants.unshift(variation);
                 }
-                variations.colors[variation.color.toLowerCase()].push(variation);
-            }
-            else if ('' + variations.type === 'size') {
-                variations.sizes.push(variation);
+                else {
+                    variants.push(variation);
+                }
+
+                if ('' + variations.type === 'color') {
+                    if (!variations.colors[variation.color.toLowerCase()]) {
+                        variations.colors[variation.color.toLowerCase()] = [];
+                    }
+                    variations.colors[variation.color.toLowerCase()].push(variation);
+                }
+                else if ('' + variations.type === 'size') {
+                    variations.sizes.push(variation);
+                }
             }
         }
+
+        if (!variants.length) {
+            variants.push(baseProductProperties);
+        } else {
+            baseProductProperties = variants[0];
+        }
+        baseProductProperties.productTrackingPath = request.getAttribute("cq.commerce.productTrackingPath");
+
+        product.base = baseProductProperties;
+        product.baseProduct = baseProduct;
+        product.variants = variants;
+        product.variations = variations;
+
+        product.variationTitle = variationTitle;
+        product.variationLead = variationLead;
+
+        product.path = baseProduct.getPath();
     }
 
-    if (!variants.length) {
-        variants.push(baseProductProperties);
-    } else {
-        baseProductProperties = variants[0];
-    }
-    baseProductProperties.productTrackingPath = request.getAttribute("cq.commerce.productTrackingPath");
-
-    product.base = baseProductProperties;
-    product.baseProduct = baseProduct;
-    product.variants = variants;
-    product.variations = variations;
-
-    product.variationTitle = variationTitle;
-    product.variationLead = variationLead;
-
-    product.path = baseProduct.getPath();
     product.redirect = redirect;
     product.errorRedirect = errorRedirect;
     product.addToCartUrl = addToCartUrl;
@@ -97,6 +98,9 @@ use(["commerce_init.js"], function (commerceInit) {
     return product;
 
     function getProductProperties(product) {
+        if (!product) {
+            return null;
+        }
         var productImage = resolver.getResource(product.getImage().getPath());
 
         return {
