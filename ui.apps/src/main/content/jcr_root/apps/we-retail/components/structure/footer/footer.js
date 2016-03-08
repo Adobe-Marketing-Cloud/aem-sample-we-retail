41ec533b887bd7c005ac3183440f7363c846a789
Bertrand de Coatpont <lebescon@adobe.com>
2016-03-08 16:12:25 +0100

    Dynamic generation of Communities area for We.Retail

--- ui.apps/src/main/content/jcr_root/apps/we-retail/components/structure/footer/footer.js
+++ ui.apps/src/main/content/jcr_root/apps/we-retail/components/structure/footer/footer.js
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2015 Adobe Systems Incorporated
+ *  Copyright 2016 Adobe Systems Incorporated
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -14,12 +14,39 @@
  *  limitations under the License.
  */
 
-// Server-side JavaScript for the main.html logic
-use(function () {
-    var Calendar = Packages.java.util.Calendar;
-    var currentYear = Calendar.getInstance().get(Calendar.YEAR);
+use(function () {  
 
-    return {
-        year: currentYear
-    };
+	'use strict';
+
+	// Checking if the Community area has been setup or not on this instance
+	var communityEnabled = false;
+	var communitySetup = '/bin/SetupCommunities?contentPath=/etc/community/we-retail';
+	var communitySignin = '/content/we-retail/community/en/signin';
+	var communityHome = '/content/we-retail/community/en';
+	var communityBlog = '/content/we-retail/community/en/blog';
+	var communityForum = '/content/we-retail/community/en/forum';
+	var communityQA = '/content/we-retail/community/en/questions';
+	var communityRoot = resolver.getResource('/content/we-retail/community');
+	if (communityRoot != null) {
+		communityEnabled = true;
+	} else {
+		communityHome = communitySetup + '&returnURL=' + communityHome + '.html#top';
+		communitySignin = communitySetup + '&returnURL=' + communitySignin + '.html#top';
+		communityBlog = communitySetup + '&returnURL=' + communityBlog + '.html#top';
+		communityForum = communitySetup + '&returnURL=' + communityForum + '.html#top';
+		communityQA = communitySetup + '&returnURL=' + communityQA + '.html#top';
+	}
+
+	var Calendar = Packages.java.util.Calendar;
+	var currentYear = Calendar.getInstance().get(Calendar.YEAR);
+	
+	return {
+		communityEnabled: communityEnabled,
+		communityHome: communityHome,
+		communityBlog: communityBlog,
+		communityForum: communityForum,
+		communityQA: communityQA,
+		year: currentYear
+	};
+
 });
